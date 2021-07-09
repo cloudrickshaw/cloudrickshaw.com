2359ed6670df4e5d47067349a44caa024fe2feaf
prabessh <prabesh.dahal@innovatetech.co>
2021-07-10 01:01:49 +0545

    Add Navbar (WIP)

---
+++ src/Nav/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+const Navbar = () => {
+	return (
+		<>
+			<div className="navbar">
+				<div className="navigations">
+					<div className="nav-item">HOME</div>
+					<div className="nav-item">F.A.Q</div>
+					<div className="nav-item">Book an Appointment</div>
+				</div>
+				<div className="social-links">"Social Links"</div>
+			</div>
+			<div className="title">Cloud Rickshaw</div>
+		</>
+	);
+};
+
+export default Navbar;
