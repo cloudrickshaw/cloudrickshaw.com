1643584d842d5a009a4bd6cc732f548119ccd6e1
prabessh <prabesh.dahal@innovatetech.co>
2021-07-10 23:10:10 +0545

    Add external link to calendly
    * Book for Appointment Functional

--- src/Nav/Navbar.jsx
+++ src/Nav/Navbar.jsx
@@ -1,14 +1,27 @@
+import { Button } from "@material-ui/core";
 import SocialLink from "./SocialLink";
 import "./styles.css";
 
 const Navbar = () => {
+	const redirectCalendly = () => {
+		window.open("https://calendly.com/cloudrickshaw");
+	};
+
 	return (
 		<>
 			<div className="navbar">
 				<div className="navigations">
-					<div className="nav-item">HOME</div>
-					<div className="nav-item">F.A.Q</div>
-					<div className="nav-item">Book an Appointment</div>
+					<div className="nav-item">
+						<Button style={{ fontWeight: "bolder" }}>HOME</Button>
+					</div>
+					<div className="nav-item">
+						<Button style={{ fontWeight: "bolder" }}>F.A.Q</Button>
+					</div>
+					<div className="nav-item">
+						<Button style={{ fontWeight: "bolder" }} onClick={redirectCalendly}>
+							Book an Appointment
+						</Button>
+					</div>
 				</div>
 				<SocialLink />
 			</div>
