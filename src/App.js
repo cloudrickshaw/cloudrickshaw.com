5041d5b10e7829ded937c857aa544186e1c61dab
prabessh <prabesh.dahal@innovatetech.co>
2021-07-10 23:53:06 +0545

    Add Sub-title Seperator for HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Navbar from "./Nav/Navbar";
+import Seperator from "./Components/Seperator";
 
 function App() {
 	return (
 		<div className="App">
 			<div className="wrapper">
 				<Navbar />
+				<Seperator title={"MISSION"} />
+				<Seperator title={"Values"} />
+				<Seperator title={"what we offer"} />
+				<Seperator title={"Team"} />
+				<Seperator title={"About"} />
+				<Seperator title={"Contacts"} />
 			</div>
 		</div>
 	);
